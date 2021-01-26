90f15d3ddb41851a03126e5bddbd013220894ea6
rubenfgr <rubenfgr@outlook.com>
2021-01-26 09:14:58 +0100

    se movió la configuración a un fichero independiente

--- src/sim/sim.cpp
+++ src/sim/sim.cpp
@@ -1,18 +1,6 @@
 #include "../sensors/sensors.h"
 #include "../debugger/debugger.h"
-
-#define GSM_PIN ""
-const char apn[] = "movistar.es";
-const char gprsUser[] = "MOVISTAR";
-const char gprsPass[] = "MOVISTAR";
-const char wifiSSID[] = "";
-const char wifiPass[] = "";
-const char *broker = "emqx.rubenfgr.com";
-const char *topicSub = "s/u/h/vi/172";
-const char *topicPub = "n/u/h/vi/172";
-const uint16_t MQTT_RECONNECT_ATTEMPTS_MAX = 3;
-const uint32_t RECCONECT_TIME = 10000;
-long PUBLISH_DATA_SPEED = 1000 * 60 * 24;
+#include "../config.h"
 
 // Select your modem:
 // #define TINY_GSM_MODEM_SIM800
